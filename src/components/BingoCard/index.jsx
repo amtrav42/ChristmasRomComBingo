1d9ca9e62de1820c9db165db206a340f2fc781cd
Amelia Travers <amelia.travers@myob.com>
2020-11-23 20:39:16 +1100

    created first component

---
+++ src/components/BingoCard/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import BingoCardStyles from "./BingoCard.module.css";
+
+export default function BingoCard() {
+  return (
+    <div className={BingoCardStyles.background}>I'm a bingo card</div>
+  )
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
+import BingoCard from "../components/BingoCard"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <BingoCard />
+  );
 }
