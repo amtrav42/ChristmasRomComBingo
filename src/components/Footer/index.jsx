627a3274dffb7a63032403f2087444bcddcfaa01
Amelia Travers <amelia.travers@myob.com>
2020-11-23 21:33:02 +1100

    styled base components

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,42 @@
 import React from "react";
 import FooterStyles from "./Footer.module.css";
+import {
+  FaHeart,
+  FaCss3Alt,
+  FaAws,
+} from 'react-icons/fa';
+import { GrGatsbyjs } from 'react-icons/gr';
 
 export default function Footer() {
   return (
-    <div className={FooterStyles.background}>I'm a footer</div>
+  <div className={FooterStyles.container}>
+    <p className={FooterStyles.acknowledgement}>
+      We would like to acknowledge the Traditional Custodians of the lands
+      on which this website was created, is hosted and is viewed.
+      This website was created on the lands of the Boon Wurrung and Woiwurrung (Wurundjeri)
+      peoples of the Kulin Nation. We pay respects to their Elders past, present and emerging,
+      and acknowledge them as the original inspiring and ongoing contributors to STEM
+      and storytelling in Australia.
+    </p>
+    <p className={FooterStyles.footer}>
+      Thank you for visiting this very silly website thrown together for the 2020 Gatsby Silly Site Competition. 
+      <br />
+      {'Made with '}
+      <span className={FooterStyles.icons}>
+        <FaHeart />
+        {' '}
+        <GrGatsbyjs />
+        {' '}
+        <FaCss3Alt />
+        {' '}
+        <FaAws />
+        {' '}
+      </span>
+      in Melbourne by
+      <a href="https://twitter.com/amaltr42" target="_blank" rel="noreferrer"> Amelia</a>
+      <br />
+      {`Copyright ${(new Date()).getFullYear()} Amelia.`}
+    </p>
+  </div>
   )
 };
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,8 @@ import HeaderStyles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <div className={HeaderStyles.background}>I'm a Header</div>
+    <div className={HeaderStyles.background}>
+      <h1>Christmas RomCom Bingo</h1>
+    </div>
   )
 };
