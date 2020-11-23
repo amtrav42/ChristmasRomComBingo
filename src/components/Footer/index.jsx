1e8ad1895a54fca8827f10b4cd688dea54e4ca5e
Amelia Travers <amelia.travers@myob.com>
2020-11-23 20:54:22 +1100

    added header and footer components

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import FooterStyles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <div className={FooterStyles.background}>I'm a footer</div>
+  )
+};
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HeaderStyles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <div className={HeaderStyles.background}>I'm a Header</div>
+  )
+};
