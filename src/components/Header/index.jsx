15d488b8f419e482fbd8e972068a666cfc2a3be1
Amelia Travers <amelia.travers@myob.com>
2020-11-24 14:07:39 +1100

    updated title

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import HeaderStyles from "./Header.module.css";
 export default function Header() {
   return (
     <div className={HeaderStyles.background}>
-      <h1>Christmas RomCom Bingo</h1>
+      <h1>A Christmas RomCom Game</h1>
     </div>
   )
 };
