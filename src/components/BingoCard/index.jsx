7d116b37eb5ad4948f483a13aa3ebeb8adf9cd96
Amelia Travers <amelia.travers@myob.com>
2020-12-05 22:59:22 +1100

    added random bingo card titles, very random...

--- src/components/BingoCard/index.jsx
+++ src/components/BingoCard/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import BingoCardStyles from "./BingoCard.module.css";
 import BingoItemList from "../../data/BingoItemList";
+import BingoTitleList from "../../data/bingoCardTitleList";
 import BingoItem from "../BingoItem";
 
 export default function BingoCard() {
@@ -8,15 +9,17 @@ export default function BingoCard() {
   const shuffled = BingoItemList.sort(() => 0.5 - Math.random());
   // Get sub-array of first n elements after shuffled
   const selectedContentList = shuffled.slice(0, 9);
+  const title = BingoTitleList[Math.floor(Math.random()*BingoTitleList.length)];
 
   return (
     <div className={BingoCardStyles.container}>
-      <div className={BingoCardStyles.title}>I'm a bingo card title</div>
+      <div className={BingoCardStyles.title}>{title}</div>
       <div className={BingoCardStyles.wrapper}>
       {
         selectedContentList.map((bingoItemProps) => (
           <BingoItem
-            content={bingoItemProps.content}
+            content={bingoItemProps.content} 
+            // isChecked
           />
         ))
       }
--- src/components/BingoItem/index.jsx
+++ src/components/BingoItem/index.jsx
@@ -4,8 +4,11 @@ import BingoItemStyles from "./BingoItem.module.css";
 export default function BingoItem(props) {
   const {
     content,
+    isChecked,
   } = props;
   return (
-    <div className={BingoItemStyles.card}>{content}</div>
+    <div className={`${BingoItemStyles.card} ${isChecked ? BingoItemStyles.isChecked : ""}`}>
+      {content}
+    </div>
   )
 };
