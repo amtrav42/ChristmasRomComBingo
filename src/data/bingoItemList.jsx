fe6d188448f244c7b8a31c1a05512b046e357372
Amelia Travers <amelia.travers@myob.com>
2020-12-06 12:24:53 +1100

    added IDs to bingo items

--- src/data/bingoItemList.jsx
+++ src/data/bingoItemList.jsx
@@ -2,120 +2,159 @@ import React from 'react';
 
 export default [
   {
-    content: 'Sage advice from a child'
+    content: 'Sage advice from a child',
+    id: 1
   },
   {
-    content: '"You can’t spend Christmas alone"'
+    content: '"You can’t spend Christmas alone"',
+    id: 2
   },
   {
-    content: 'Someone gets hot chocolate'
+    content: 'Someone gets hot chocolate',
+    id: 3
   },
   {
-    content: 'Mistletoe'
+    content: 'Mistletoe',
+    id: 4
   },
   {
-    content: 'Christmas Cookies'
+    content: 'Christmas Cookies',
+    id: 5
   },
   {
-    content: 'Almost first kiss gets interrupted'
+    content: 'Almost first kiss gets interrupted',
+    id: 6
   },
   {
-    content: 'International Travel'
+    content: 'International Travel',
+    id: 7
   },
   {
-    content: 'Video chat with parent'
+    content: 'Video chat with parent',
+    id: 8
   },
   {
-    content: 'Instant dislike of future romantic partner'
+    content: 'Instant dislike of future romantic partner',
+    id: 9
   },
   {
-    content: 'Sage advice from random old person'
+    content: 'Sage advice from random old person',
+    id: 10
   },
   {
-    content: '"I was a fool for thinking this could work”'
+    content: '"I was a fool for thinking this could work”',
+    id: 11
   },
   {
-    content: 'Character watches Netflix'
+    content: 'Character watches Netflix',
+    id: 12
   },
   {
-    content: '“We’ve been friends forever”'
+    content: '“We’ve been friends forever”',
+    id: 13
   },
   {
-    content: '“We can’t do that because tradition”'
+    content: '“We can’t do that because tradition”',
+    id: 14
   },
   {
-    content: '“We have to do that because tradition”'
+    content: '“We have to do that because tradition”',
+    id: 15
   },
   {
-    content: 'Man is shirtless or removes shirt'
+    content: 'Man is shirtless or removes shirt',
+    id: 16
   },
   {
-    content: 'Surprise visitor from out of town'
+    content: 'Surprise visitor from out of town',
+    id: 17
   },
   {
-    content: '“Mean” girl is mean'
+    content: '“Mean” girl is mean',
+    id: 18
   },
   {
-    content: 'Drama is resolved'
+    content: 'Drama is resolved',
+    id: 19
   },
   {
-    content: 'Phone Call with sister'
+    content: 'Phone Call with sister',
+    id: 20
   },
   {
-    content: 'Secret long term crush'
+    content: 'Secret long term crush',
+    id: 21
   },
   {
-    content: 'Royalty'
+    content: 'Royalty',
+    id: 22
   },
   {
-    content: 'All hope is lost'
+    content: 'All hope is lost',
+    id: 23
   },
   {
-    content: 'Duty is mentioned'
+    content: 'Duty is mentioned',
+    id: 24
   },
   {
-    content: 'Destiny is mentioned'
+    content: 'Destiny is mentioned',
+    id: 25
   },
   {
-    content: '“The meaning of Christmas”'
+    content: '“The meaning of Christmas”',
+    id: 26
   },
   {
-    content: 'Scrooge like character'
+    content: 'Scrooge like character',
+    id: 27
   },
   {
-    content: 'Charity work'
+    content: 'Charity work',
+    id: 28
   },
   {
-    content: 'Children in need'
+    content: 'Children in need',
+    id: 29
   },
   {
-    content: 'Someone falls over in the snow'
+    content: 'Someone falls over in the snow',
+    id: 30
   },
   {
-    content: 'Snowball fight'
+    content: 'Snowball fight',
+    id: 31
   },
   {
-    content: 'Public declaration of love'
+    content: 'Public declaration of love',
+    id: 32
   },
   {
-    content: '“Actually he’s a really good guy”'
+    content: '“Actually he’s a really good guy”',
+    id: 33
   },
   {
-    content: 'A big storm'
+    content: 'A big storm',
+    id: 34
   },
   {
-    content: 'Eggnog is mentioned'
+    content: 'Eggnog is mentioned',
+    id: 35
   },
   {
-    content: 'Christmas = Snow'
+    content: 'Christmas = Snow',
+    id: 36
   },
   {
-    content: 'Wistful look'
+    content: 'Wistful look',
+    id: 37
   },
   {
-    content: 'Impractical highheels'
+    content: 'Impractical highheels',
+    id: 38
   },
   {
-    content: 'Random non-American accent'
+    content: 'Random non-American accent',
+    id: 39
   },
 ]
