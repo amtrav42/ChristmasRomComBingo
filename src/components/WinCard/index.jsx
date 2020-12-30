ccee63e03bd83db05235673dd6a725c4b6c95737
Amelia Travers <amelia.travers@myob.com>
2020-12-30 18:01:32 +1100

    fixed indenting

--- src/components/WinCard/index.jsx
+++ src/components/WinCard/index.jsx
@@ -6,44 +6,44 @@ export default function WinCard() {
 
   return (
     <div className={WinStyles.container}>
-<div className={WinStyles.snowflakes} aria-hidden="true">
-  <div className={WinStyles.snowflake}>
-  ❅
-  </div>
-  <div className={WinStyles.snowflake}>
-  ❆
-  </div>
-  <div className={WinStyles.snowflake}>
-  ❅
-  </div>
-  <div className={WinStyles.snowflake}>
-  ❆
-  </div>
-  <div className={WinStyles.snowflake}>
-  ❅
-  </div>
-  <div className={WinStyles.snowflake}>
-  ❆
-  </div>
-  <div className={WinStyles.snowflake}>
-    ❅
-  </div>
-  <div className={WinStyles.snowflake}>
-    ❆
-  </div>
-  <div className={WinStyles.snowflake}>
-    ❅
-  </div>
-  <div className={WinStyles.snowflake}>
-    ❆
-  </div>
-  <div className={WinStyles.snowflake}>
-    ❅
-  </div>
-  <div className={WinStyles.snowflake}>
-    ❆
-  </div>
-</div>
+      <div className={WinStyles.snowflakes} aria-hidden="true">
+        <div className={WinStyles.snowflake}>
+        ❅
+        </div>
+        <div className={WinStyles.snowflake}>
+        ❆
+        </div>
+        <div className={WinStyles.snowflake}>
+        ❅
+        </div>
+        <div className={WinStyles.snowflake}>
+        ❆
+        </div>
+        <div className={WinStyles.snowflake}>
+        ❅
+        </div>
+        <div className={WinStyles.snowflake}>
+        ❆
+        </div>
+        <div className={WinStyles.snowflake}>
+          ❅
+        </div>
+        <div className={WinStyles.snowflake}>
+          ❆
+        </div>
+        <div className={WinStyles.snowflake}>
+          ❅
+        </div>
+        <div className={WinStyles.snowflake}>
+          ❆
+        </div>
+        <div className={WinStyles.snowflake}>
+          ❅
+        </div>
+        <div className={WinStyles.snowflake}>
+          ❆
+        </div>
+      </div>
       <div className={WinStyles.wrapper}>
         <div className={WinStyles.title}>You Won!</div>
         <div className={WinStyles.subtitle}>Congratulations</div>
